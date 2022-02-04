ca378d8d7cb098488f4c417bbb9e263f6237467d
Anastasia <mmmargoshaaa.com>
2022-02-04 20:05:40 +0500

    2

--- Desktop/Repositories/task2/Program.cs
+++ Desktop/Repositories/task2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Даны два числа. Показать большее и меньшее число
+
+Console.WriteLine("Введите первое число : ");
+int a = Convert.ToUInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число : ");
+int b = Convert.ToUInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);
+}
+if (a < b) 
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+    Console.Write("min = ");
+    Console.WriteLine(b);   
+}
+
+
