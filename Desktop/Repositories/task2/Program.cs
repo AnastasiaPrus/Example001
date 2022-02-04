acbd76e95687a45d9d3cf0804802f5ae5f542858
Anastasia <mmmargoshaaa.com>
2022-02-04 20:21:12 +0500

    Задача 2: Даны два числа. Показать большее и меньшее число

--- Desktop/Repositories/task2/Program.cs
+++ Desktop/Repositories/task2/Program.cs
@@ -1,24 +1,19 @@
 ﻿// Задача 2: Даны два числа. Показать большее и меньшее число
 
-Console.WriteLine("Введите первое число : ");
-int a = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите второе число : ");
-int b = Convert.ToUInt32(Console.ReadLine());
-
-if (a > b)
+if (n>m)
 {
-    Console.Write("max = ");
-    Console.WriteLine(a);
-    Console.Write("min = ");
-    Console.WriteLine(b);
+    Console.WriteLine("max= "+ n );
+    Console.WriteLine("min= "+ m );
 }
-if (a < b) 
+else 
 {
-    Console.Write("max = ");
-    Console.WriteLine(a);
-    Console.Write("min = ");
-    Console.WriteLine(b);   
+    Console.WriteLine("max= "+ m );
+    Console.WriteLine("min= "+ n );
 }
 
 
