65dff7bddc9eaddbf78a1a4f3c64f0eeeab7366a
Anastasia <mmmargoshaaa.com>
2022-02-08 16:49:02 +0500

    10

---
+++ Desktop/Repositories/task10/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+
+Console.WriteLine("Введите трёхзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine((a%100)/10);
