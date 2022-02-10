66e1b1e022a7bb7bc95a721b932156c4d88da7a0
Anastasia <mmmargoshaaa.com>
2022-02-10 15:53:20 +0500

    18

---
+++ Desktop/Repositories/task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Даны два числа. Показать большее и меньшее число
+
+//Console.WriteLine("Введите число: ");
+//int n = int.Parse(Console.ReadLine());
+
+
+//Console.WriteLine("max= " n);
+//Console.WriteLine("min= " b);
+
+// Вывести квадрат числа
+// int n = new Random().Next(1, 10);
+Console.WriteLine("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+
+if (n>m)
+{
+    Console.WriteLine("max= "+ n );
+    Console.WriteLine("min= "+ m );
+}
+else 
+{
+    Console.WriteLine("max= "+ m );
+    Console.WriteLine("min= "+ n );
+}
---
+++ Desktop/Repositories/task18/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 18: Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool x = true;
+bool y = false;
+
+if ((!(x || y)) == (!x && !y))
+{
+    Console.WriteLine("Выражение истинно");
+}
+else
+{
+    Console.WriteLine("Выражение ложно");
+}
+
+bool x1 = false;
+bool y1 = true;
+
+if ((!(x1 || y1)) == (!x1 && !y1))
+{
+    Console.WriteLine("Выражение истинно");
+}
+else
+{
+    Console.WriteLine("Выражение ложно");
+}
+
+bool x2 = true;
+bool y2 = true;
+
+if ((!(x2 || y2)) == (!x2 && !y2))
+{
+    Console.WriteLine("Выражение истинно");
+}
+else
+{
+    Console.WriteLine("Выражение ложно");
+}
+
+bool x3 = false;
+bool y3 = false;
+
+if ((!(x3 || y3)) == (!x3 && !y3))
+{
+    Console.WriteLine("Выражение истинно");
+}
+else
+{
+    Console.WriteLine("Выражение ложно");
+}
