92cf09cf634d0e7dcfc8b4f35101763660f2988c
Anastasia <mmmargoshaaa.com>
2022-02-10 18:18:44 +0500

    20

--- Desktop/Repositories/seminar3/Program.cs
+++ Desktop/Repositories/seminar3/Program.cs
@@ -7,11 +7,55 @@
 
 // Задача 23: Показать таблицу квадратов чисел от 1 до N
 
+// Задача 17: По двум заданным числам проверять является ли одно квадратом другого.
 
-//17
+// int a = new Random().Next(1, 99);
+// int b = new Random().Next(1, 99);
 
-//19
+int a = 5;
+int b = 25;
 
-//21
+// if (a == b*b || b == a*a)
+// {
+//     Console.WriteLine("является");
+// }
 
-//23
+// else
+// {
+//     Console.WriteLine("не является");
+// }
+
+// Console.WriteLine( "a=" + a + " b=" + b);
+
+if (a == b*b) Console.WriteLine($"Число {a} является квадратом числа {b}");
+else Console.WriteLine($"Число {a} не является квадратом числа {b}");
+
+if (b == a*a) Console.WriteLine($"Число {b} является квадратом числа {a}");
+else Console.WriteLine($"Число {b} не является квадратом числа {a}");
+
+
+// Задача 19: Определить номер четверти плоскости, 
+    //в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int x = new Random().Next(-99, 99);
+int y = new Random().Next(-99, 99);
+Console.WriteLine( "x=" + x + " y=" + y);
+
+if (x != 0 && y != 0)
+{
+if (x>0 && y>0) Console.WriteLine("Точка в первой четверти");
+if (x<0 && y>0) Console.WriteLine("Точка во второй четверти");
+if (x<0 && y<0) Console.WriteLine("Точка в третьей четверти");
+if (x>0 && y<0) Console.WriteLine("Точка в четвертой четверти");
+}
+else Console.WriteLine("Точка на оси координат");
+
+// Задача 21: Программа проверяет пятизначное число на палиндромом. 12321
+
+Console.WriteLine("Введите пятизначное число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n/10000 == n%10 && n/1000%10 == n%100/10) Console.WriteLine("Число палиндром");
+else Console.WriteLine("Число не является палиндромом");
+
+// Задача 23: Показать таблицу квадратов чисел от 1 до N
--- Desktop/Repositories/task20/Program.cs
+++ Desktop/Repositories/task20/Program.cs
@@ -1,1 +1,10 @@
 ﻿// Задача 20: Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти: 1, 2, 3, 4 ");
+int x = int.Parse(Console.ReadLine());
+
+if (x==1) Console.WriteLine("Диапазон координат (x>0; y>0)");
+if (x==2) Console.WriteLine("Диапазон координат (x<0; y>0)");
+if (x==3) Console.WriteLine("Диапазон координат (x<0; y<0)");
+if (x==4) Console.WriteLine("Диапазон координат (x>0; y<0)");
+
